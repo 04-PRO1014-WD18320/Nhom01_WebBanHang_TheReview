4e8d8fdbae17cdd8f08d736ea5bc3cd0d809ac1a
denker9999 <52444982+phihoang1709@users.noreply.github.com>
2023-11-22 17:54:16 +0700

    a
    a

--- view_site/dang_ky_dang_nhap/dang_nhap.php
+++ view_site/dang_ky_dang_nhap/dang_nhap.php
@@ -11,11 +11,11 @@ background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);
                 <h1>Đăng nhập</h1>
                 <div class="form-group">
                     <h5>Tài khoản</h5>
-                    <input type="text" name="" id="validationCustom01" class="form-control my-2" placeholder="Vui lòng nhập tài khoản" >
+                    <input type="text" name="emai_dang_nhap" id="validationCustom01" class="form-control my-2" placeholder="Vui lòng nhập tài khoản" >
                     
                     <h5>Mật khẩu</h5>
                     
-                    <input type="password" name="" id="" class="form-control my-2" placeholder="Vui lòng nhập mật khẩu" >
+                    <input type="password" name="mat_khau_dang_nhap" id="" class="form-control my-2" placeholder="Vui lòng nhập mật khẩu" >
                     <a href="./index.php" class="my-2">Quên mật khẩu ?</a>
                 </div>
                 <button type="submit" class="btn btn-danger my-3" name="action" value="dang_nhap">Đăng nhập</button>
